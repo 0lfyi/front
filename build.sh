9ea7fbc6c68a2a6730639c40d47acb9048f92e68
William Riancho <william@reptilians.io>
2022-12-21 15:45:19 +0700

    fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 COMMIT_HASH=$(git log --pretty=oneline | head -1 | cut -d ' ' -f 1)
+CI_COMMIT_SHA="${COMMIT_HASH:0:8}"
 
-docker build -f ./docker/Dockerfile -t "0l.fyi/front:${COMMIT_HASH:0:8}" .
+docker build \
+  -f ./docker/Dockerfile \
+  --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
+  -t "0l.fyi/front:${CI_COMMIT_SHA}" \
+  .
 
